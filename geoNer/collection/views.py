a18335d024d9320bdffb7c236b5335f8f3101de6
FlorianKrull <floriankrull1507@yahoo.de>
2019-10-31 19:31:37 +0100

    Added first version of spacy analyzer

--- geoNer/collection/views.py
+++ geoNer/collection/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from .models import Collection, Document, Version
 from django.contrib.auth.models import User
 from .forms import NewDocForm
+from django.contrib.auth.decorators import login_required
 
+import spacy
+from spacy import displacy
 # Create your views here.
 def home(request):
     collections = Collection.objects.all()
@@ -13,24 +16,82 @@ def collection_docs(request, pk):
     collection = Collection.objects.get(pk=pk)
     return render(request, 'docs.html', {'collection': collection})
 
+@login_required
 def new_doc(request, pk):
     collection = get_object_or_404(Collection, pk=pk)
-    user = User.objects.first()
     if request.method == 'POST':
         form = NewDocForm(request.POST)
 
         if form.is_valid():
             document = form.save(commit=False)
             document.collection = collection
-            document.starter = user
+            document.starter = request.user
             document.save()
 
             version = Version.objects.create(
                 text = form.cleaned_data.get('text'),
                 document = document,
-                created_by = user
+                created_by=request.user
             )
             return redirect('collection_docs', pk=collection.pk) 
     else:
         form = NewDocForm()
     return render(request, 'new_doc.html', {'collection': collection})
+
+def document_version(request, pk, document_pk):
+    document = get_object_or_404(Document, collection__pk=pk, pk=document_pk)
+    return render(request, 'document_version.html', {'document': document})
+
+
+def ner_detection(request, pk, document_pk):
+    #nlp = spacy.load("en_core_web_sm")
+    #doc = nlp(text)
+    #print(displacy.render(doc, style="ent",page=True))
+    #return displacy.render(doc, style="ent",page=True)
+    document = get_object_or_404(Document, collection__pk=pk, pk=document_pk)
+    return render(request, 'ner_detection.html', {'document': document})
+
+
+def visualize_view(request):
+    ret = {}
+    text = request.POST.get('sentences')
+    if (text is None):
+        return render(request, 'nlp/visualize_error.html')
+    markup = visualize_text(text)
+    ret['json'] = markup
+    return render(request, 'nlp/visualize.html', ret)
+
+
+def analyze(request):
+    'API text analyze view'
+    if request.method == 'POST':
+        text = request.body.decode('utf-8')
+        try:
+            text = json.loads(text)['text']
+        except ValueError:
+            # catch POST form as well
+            for key in request.POST.dict().keys():
+                text = key
+
+        if settings.ALLOW_URL_IMPORTS and text.startswith(('http://', 'https://', 'www')):
+            page = requests.get(text)
+            doc = Document(page.text)
+            soup = BeautifulSoup(doc.summary())
+            text = soup.get_text()
+            title = doc.title().strip()
+            text = '{0}.\n{1}'.format(title, text)
+
+        if not text:
+            response = JsonResponse(
+                {'status': 'false', 'message': 'need some text here!'})
+            response.status_code = 400
+            return response
+
+        # add some limit here
+        text = text[:200000]
+        ret = {}
+        ret = analyze_text(text)
+        return JsonResponse(ret)
+    else:
+        ret = {'methods_allowed': 'POST'}
+        return JsonResponse(ret)
--- geoNer/geoNer/urls.py
+++ geoNer/geoNer/urls.py
@@ -26,7 +26,33 @@ urlpatterns = [
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^collection/(?P<pk>\d+)/$', views.collection_docs, name='collection_docs'),
+    url(r'^collection/(?P<pk>\d+)/$',
+        views.collection_docs, name='collection_docs'),
+    url(r'^collection/(?P<pk>\d+)/document/(?P<document_pk>\d+)/$', views.document_version, name='document_version'),
     url(r'^collection/(?P<pk>\d+)/new/$', views.new_doc, name='new_doc'),
     url(r'^admin/', admin.site.urls),
+    url(r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+        ),
+        name='password_reset'),
+    url(r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='password_reset_done.html'),
+            name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html'),
+            name='password_reset_confirm'),
+    url(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html'),
+            name='password_reset_complete'),
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+            name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+            name='password_change_done'),
+    url(r'^collection/(?P<pk>\d+)/document/(?P<document_pk>\d+)/ner_detection/$', views.ner_detection, name='ner_detection'),
 ]
